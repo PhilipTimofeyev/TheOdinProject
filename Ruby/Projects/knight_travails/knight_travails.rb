698762b7b5b8e6a11e6a2752172051cdab380d40
PhilipTimofeyev <PhilipTimofeyev@gmail.com>
2023-12-11 23:17:06 -0800

    Refactor and added knight class with knight moveset

--- Ruby/Projects/knight_travails/knight_travails.rb
+++ Ruby/Projects/knight_travails/knight_travails.rb
@@ -1,3 +1,4 @@
+
 class Node
   attr_accessor :adjacents, :visited, :draw_square
   attr :coordinate
@@ -14,14 +15,31 @@ class Node
   end
 end
 
+class Knight
+  def moveset(current_square)
+    adjacent_squares = []
+    x = current_square.coordinate.first
+    y = current_square.coordinate.last
+
+    x_moves = [2, 2, -2, -2, 1, -1, 1, -1] 
+    y_moves = [1, -1, 1, -1, 2, 2, -2, -2]
+
+    x_moves.each_with_index{|n, idx| adjacent_squares << [x + n, y + y_moves[idx]]}
+
+    adjacent_squares
+  end
+end
+
 class Chessboard
   LOWER_BOUND = 0
   UPPER_BOUND = 7
 
-  attr :board
+  attr :board, :knight
 
   def initialize
     @board = {}
+    @knight = Knight.new
+    build_board
   end
 
   def add_node(node)
@@ -34,48 +52,29 @@ class Chessboard
         add_node(Node.new(row, column))
       end
     end
-    add_all_adjacent_squares
   end
 
   def draw_board
     clear
     row_count = UPPER_BOUND
 
-    board.values.reverse.each_slice(UPPER_BOUND + 1) do |n| 
-      puts "#{row_count}|#{n.reverse.map{|n| n.draw_square}.join("|")}|" 
+    board.values.reverse.each_slice(UPPER_BOUND + 1) do |row| 
+      puts "#{row_count}|#{row.reverse.map{|square| square.draw_square}.join("|")}|" 
       row_count -= 1
     end
 
     puts "  " + (0..UPPER_BOUND).map{|n| n.to_s}.join(" ") #labels column numbers
   end
 
-  def find_adjacents(square)
-    adjacent_squares = []
-    x = square.coordinate.first
-    y = square.coordinate.last
-
-    x_moves = [2, 2, -2, -2, 1, -1, 1, -1] 
-    y_moves = [1, -1, 1, -1, 2, 2, -2, -2]
-
-    x_moves.each_with_index{|n, idx| adjacent_squares << [x + n, y + y_moves[idx]]}
-
-    adjacent_squares.select {|n| within_bounds?(n)}
-  end
-
-  def add_all_adjacent_squares #convert adjacent coordinates back to node objects and add to node.adjacents
-    board.each do |coord, square|
-      find_adjacents(square).each do |adj_square|
-        board[adj_square].add_adjacent(square)
-      end
-    end
+  def validate_moves(moveset)
+    moveset.select {|coord| within_bounds?(coord)}.map{|coord| board[coord]}
   end
 
   def within_bounds?(coordinate)
-    coordinate.none? {|n| n < Chessboard::LOWER_BOUND || n > Chessboard::UPPER_BOUND}
+    coordinate.none? {|xy| xy < Chessboard::LOWER_BOUND || xy > Chessboard::UPPER_BOUND}
   end
 
   def find_shortest_path(starting_coordinate, final_coordinate)
-
     starting_node = board[starting_coordinate]
     final_node = board[final_coordinate]
 
@@ -85,7 +84,7 @@ class Chessboard
     until queue.empty?
       current_node = queue.shift
       current_node.visited = true
-      current_node.adjacents.each do |adjacent_square|
+      validate_moves(knight.moveset(current_node)).each do |adjacent_square|
         unless adjacent_square.visited
           queue.push(adjacent_square)
           previous_node_table[adjacent_square.coordinate] = current_node.coordinate
@@ -97,17 +96,17 @@ class Chessboard
     current_node_coordinate = final_node.coordinate
 
     while current_node_coordinate != starting_coordinate
-      shortest_path << current_node_coordinate
+      shortest_path.prepend(current_node_coordinate)
       current_node_coordinate = previous_node_table[current_node_coordinate]
     end
-
-    shortest_path << starting_coordinate
-
-    shortest_path.reverse
+    shortest_path.prepend(starting_coordinate)
   end
 
   def reset_board
-    board.values.each {|square| square.visited = false}
+    board.values.each do |square| 
+      square.visited = false
+      square.draw_square = '_'
+    end
   end
 
   def clear
@@ -115,7 +114,6 @@ class Chessboard
   end
 
   def knight_moves(starting_coordinate, final_coordinate)
-    build_board
     board[starting_coordinate].draw_square = "S"
     board[final_coordinate].draw_square = "E"
 
@@ -135,7 +133,5 @@ class Chessboard
   end
 end
 
-
-
 chess = Chessboard.new
-chess.knight_moves([0, 7], [7, 0]) 
+chess.knight_moves([0, 0], [7, 7]) 
