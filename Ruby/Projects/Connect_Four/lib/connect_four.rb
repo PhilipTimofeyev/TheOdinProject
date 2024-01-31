c3a90a7eaa35140d3b8c59d02b60db4e39a69685
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-01-31 11:06:00 -0800

    Added tests and methods for winning conditions

--- Ruby/Projects/Connect_Four/lib/connect_four.rb
+++ Ruby/Projects/Connect_Four/lib/connect_four.rb
@@ -27,6 +27,7 @@ end
 
 
 class Board
+	EMPTY_SLOT = '__'
 	attr_accessor :slots
 
 	def initialize
@@ -34,17 +35,171 @@ class Board
 	end
 
 	def create_slots
-		6.times {self.slots << create_row}
+		6.times { @slots << create_row }
 	end
 
 	def create_row
 		row = []
-		7.times {row << '_'}
+		7.times { row << EMPTY_SLOT }
 
 		row
 	end
 
+	def draw_board
+		puts "_" * 30
+		slots.each do |row| 
+			puts " "
+			puts "‖ " + row.join("  ") + " ‖"
+		end
+		puts "―" * 30
+	end
 
+	def []=(column, token)
+		row, column = next_available_slot(column)
 
+		@slots[row][column] = token
+	end
+
+	def next_available_slot(column)
+		5.downto(0) do |row|
+			slot = slots[row][column]
+			return [row, column] if slot == EMPTY_SLOT 
+		end
+	end
+
+	def reset_board
+		@slots = []
+		create_slots
+	end
+
+	def winner?
+		horizontal_winner || vertical_winner || diagonal_winner || diagonal_winner_transposed
+
+	end
+
+	def horizontal_winner(board_slots = slots)
+		board_slots.each do |row|
+			row.each_cons(4) do |set|
+				return "⚪" if set.all? { |slot| slot.token == "⚪" if slot != EMPTY_SLOT }
+				return "⚫" if set.all? { |slot| slot.token == "⚫" if slot != EMPTY_SLOT }
+			end
+		end
+		false
+	end
+
+	def vertical_winner(board_slots = slots)
+		 transposed = slots.transpose
+		 horizontal_winner(transposed)
+	end
+
+	def diagonal_left_right_winner
+		starter = 0
+		row = 0
+		column = 0
+		result = []
+
+		loop do
+			set = []
+
+			loop do
+				set << [row, column]
+				row += 1
+				column += 1
+				if column > 6 || row > 5
+					row = 0
+					column = starter
+					break
+				end
+			end
+
+			result << set
+			set = []
+			starter += 1
+			column += 1
+			break if starter == 4
+		end	
+
+		result
+	end
+
+	def diagonal_right_left_winner
+		diag_line = diagonal_left_right_winner.dup
+
+		diag_line.each do |n| 
+			n.map! do |a| 
+				a.reverse
+			end
+		end
+
+		diag_line[1..-1].each do |set|
+			set.reject! {|row, column| row >= 6}
+		end
+	end
+
+	def diagonal_winner(board_slots = slots)
+		diag = diagonal_left_right_winner + diagonal_right_left_winner
+
+		diag.each {|n| n}
+		diag.each do |set|
+			set.map! { |n| board_slots[n[0]][n[1]] }
+		end
+		# p diag
+		diag.each do |row|
+			row.each_cons(4) do |set|
+				return "⚪" if set.all? { |slot| slot.token == "⚪" if slot != EMPTY_SLOT }
+				return "⚫" if set.all? { |slot| slot.token == "⚫" if slot != EMPTY_SLOT }
+			end
+		end
+		false
+	end
+
+	def diagonal_winner_transposed
+		diag = diagonal_left_right_winner + diagonal_right_left_winner
+		reversed_board = slots.map {|n| n.reverse}
+
+		diag.each {|n| n}
+		diag.each do |set|
+			set.map! { |n| reversed_board[n[0]][n[1]] }
+		end
+		# p diag
+		diag.each do |row|
+			row.each_cons(4) do |set|
+				return "⚪" if set.all? { |slot| slot.token == "⚪" if slot != EMPTY_SLOT }
+				return "⚫" if set.all? { |slot| slot.token == "⚫" if slot != EMPTY_SLOT }
+			end
+		end
+		false
+	end
 
 end
+
+
+
+ board = Board.new
+ board.create_slots
+ # x.draw_board
+# x.slots[2][0] = WhiteToken.new
+# x.slots[3][1] = WhiteToken.new
+# x.slots[4][2] = WhiteToken.new
+# x.slots[5][3] = WhiteToken.new
+
+board.slots[1][0] = WhiteToken.new
+board.slots[2][1] = WhiteToken.new
+board.slots[3][2] = WhiteToken.new
+board.slots[4][3] = BlackToken.new
+board.slots[5][4] = WhiteToken.new
+
+
+ board.draw_board
+p board.diagonal_left_right_winner
+# x.diagonal_right_left_winner
+# p x.diagonal_winner
+# p x.diagonal_winner_transposed
+ p board.winner?
+
+# p x.vertical_winner
+# x.draw_board
+
+
+# x = [1, 1, 1, 2]
+# p x.all? {|n| n == 1 || n == 2}
--- Ruby/Projects/Connect_Four/spec/connect_four_spec.rb
+++ Ruby/Projects/Connect_Four/spec/connect_four_spec.rb
@@ -2,9 +2,10 @@ require_relative '../lib/connect_four'
 
 describe Board do
 	subject(:board) {described_class.new}
+	before { board.create_slots}
 
 	describe 'creates board' do
-		empty_slot = '_'
+		empty_slot = '__'
 
 		it 'creates a row of 7 empty slots' do
 			slots_in_a_row = 7
@@ -17,7 +18,6 @@ describe Board do
 		it 'creates a 42 slots' do
 			correct_num_slots = 42
 
-			board.create_slots
 			board_slots = board.slots.flatten
 
 			expect(board_slots).to include(empty_slot).exactly(42).times
@@ -26,10 +26,183 @@ describe Board do
 		it 'creates 6 rows' do
 			correct_num_rows = 6
 
-			board.create_slots
 			num_rows = board.slots.size
 
 			expect(num_rows).to be (correct_num_rows)
+		end
+	end
+
+	describe 'find next available slot' do
+
+		it 'when rows are empty' do
+			selected_column = 3
+			available_slot = board.next_available_slot(selected_column)
+			correct_slot = [5, 3]
+
+			expect(available_slot).to eq(correct_slot) 
+		end
+
+		it 'when column contains tokens' do
+			selected_column = 5
+			token = WhiteToken.new
+			board.slots[5][5] = token
+			board.slots[4][5] = token
+			board.slots[3][5] = token
+
+			correct_slot = [2, 5]
+			available_slot = board.next_available_slot(selected_column)
+
+			expect(available_slot).to eq(correct_slot) 
+		end
+	end
+
+	describe 'add_token' do
+
+		let(:board) {described_class.new}
+
+		before do 
+			board.create_slots
+		end
+
+		it 'adds correct token to specified slot when column empty' do
+
+			selected_column = 5
+			white_token = WhiteToken.new
+
+			board[selected_column] = white_token
+			correct_slot = board.slots[5][5]
+
+			expect(correct_slot.token).to eq("⚪")
+		end
+
+		it 'adds correct token to specified slot when column not empty' do
+			
+			black_token = BlackToken.new
+			white_token = WhiteToken.new
+
+			board.slots[5][4] = black_token
+			board.slots[4][4] = black_token
+
+			selected_column = 4
+
+			board[selected_column] = white_token
+			correct_slot = board.slots[3][4]
+
+			expect(correct_slot.token).to eq("⚪")
+		end
+	end
+
+	describe 'resets_board' do
+		it 'resets board to all empty slots' do
+			token = WhiteToken.new
+
+			board.slots.each_with_index do |row, row_idx|
+				row.each_with_index {|column, column_idx| board.slots[row_idx][column_idx] = token}
+			end
+
+			board.reset_board
+			slots = board.slots.flatten
+
+			expect(slots).to all(eq(Board::EMPTY_SLOT))
+		end
+	end
+
+	describe 'winning conditions' do
+		let(:white_token) { WhiteToken.new }
+		let(:black_token) { BlackToken.new }
+
+		before do 
+			board.create_slots 
+		end
+
+		example 'four adjacent horizontally' do
+			board.slots[5][0] = white_token
+			board.slots[5][1] = white_token
+			board.slots[5][2] = white_token
+			board.slots[5][3] = white_token
+
+			expect(board.winner?).to eq("⚪")
+		end
+
+		example 'four adjacent vertically' do
+			board.slots[5][0] = black_token
+			board.slots[4][0] = black_token
+			board.slots[3][0] = black_token
+			board.slots[2][0] = black_token
+
+			expect(board.winner?).to eq("⚫")
+		end
+
+		example 'bottom left to top right in a row' do
+			board.slots[2][5] = WhiteToken.new
+			board.slots[5][2] = WhiteToken.new
+			board.slots[4][3] = WhiteToken.new
+			board.slots[3][4] = WhiteToken.new
+
+			expect(board.winner?).to eq("⚪")
+		end
+
+		example 'bottom left to top right not in a row' do
+			board.slots[1][6] = WhiteToken.new
+			board.slots[5][2] = WhiteToken.new
+			board.slots[4][3] = WhiteToken.new
+			board.slots[3][4] = WhiteToken.new
+
+			expect(board.winner?).to be false
+		end
+
+		example '3 white and 1 black in a row' do
+			board.slots[2][5] = BlackToken.new
+			board.slots[5][2] = WhiteToken.new
+			board.slots[4][3] = WhiteToken.new
+			board.slots[3][4] = WhiteToken.new
+
+			expect(board.winner?).to be false
+		end
+
+		example 'top left to bottom right 4 in a row' do
+			board.slots[0][0] = WhiteToken.new
+			board.slots[1][1] = WhiteToken.new
+			board.slots[2][2] = WhiteToken.new
+			board.slots[3][3] = WhiteToken.new
+
+			expect(board.winner?).to eq("⚪")
+		end
+
+		example 'top left to bottom right 4 in a row top quadrant' do
+			board.slots[0][3] = WhiteToken.new
+			board.slots[1][4] = WhiteToken.new
+			board.slots[2][5] = WhiteToken.new
+			board.slots[3][6] = WhiteToken.new
+
+			expect(board.winner?).to eq("⚪")
+		end
+
+		example 'top left to bottom right 3 in a row' do
+			board.slots[1][0] = WhiteToken.new
+			board.slots[2][1] = WhiteToken.new
+			board.slots[3][2] = WhiteToken.new
+
+			expect(board.winner?).to be false
+		end
+
+		example 'top left to bottom right 4 not in a row' do
+			board.slots[1][0] = WhiteToken.new
+			board.slots[2][1] = WhiteToken.new
+			board.slots[3][2] = WhiteToken.new
+			board.slots[5][4] = WhiteToken.new
+
+			expect(board.winner?).to be false
+		end
+
+		example 'top left to bottom right 5 with one different token' do
+			board.slots[1][0] = WhiteToken.new
+			board.slots[2][1] = WhiteToken.new
+			board.slots[3][2] = WhiteToken.new
+			board.slots[4][3] = BlackToken.new
+			board.slots[5][4] = WhiteToken.new
+
+			expect(board.winner?).to be false
 		end
 	end
 end
